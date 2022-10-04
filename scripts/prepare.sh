c6adda03956a8a77b64e623474c0ee5b657383ce
Хрянин Владимир <v.xryanin@sigma-it.ru>
2022-10-04 08:08:06 +0700

    fix tmk_core/common/keyboard.h

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,3 +5,4 @@
 ### modify QMK
 cp ${WORK_DIR}/moonlander/quantum/keymap_common.c ${QMK_DIR}/quantum/keymap_common.c
 cp ${WORK_DIR}/moonlander/tmk_core/common/action.c ${QMK_DIR}/tmk_core/common/action.c
+cp ${WORK_DIR}/moonlander/tmk_core/common/keyboard.h ${QMK_DIR}/tmk_core/common/keyboard.h
