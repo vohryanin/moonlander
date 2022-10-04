3ebfe9d36a05a51b68d34b9bf22b52d983db9bf4
Хрянин Владимир <v.xryanin@sigma-it.ru>
2022-10-04 07:55:56 +0700

    fix prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,13 +2,5 @@
 
 . ./vars.sh
 
-### cloning repo
-mkdir -p ${WORK_DIR}
-cd ${WORK_DIR}
-find ./${REPO}/ -type f -delete
-find ./${REPO}/ -type d -delete
-#rm -rf ./${REPO}/
-git clone -b ${BRANCH} ${VCS_URL}/${REPO}.git
-
 ### modify QMK
 cp ${WORK_DIR}/moonlander/quantum/keymap_common.c ${QMK_DIR}/quantum/keymap_common.c
