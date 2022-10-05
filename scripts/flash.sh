2204b3184af66b8fc0b3535656f64bfdf8aef46a
Хрянин Владимир <v.xryanin@sigma-it.ru>
2022-10-05 18:26:40 +0700

    fix скрипты модификации QMK

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -5,3 +5,4 @@
 ### make image and flash
 cd ${QMK_DIR}
 make moonlander:optozorax:flash
+chown -R v:v ${QMK_DIR}
--- scripts/make.sh
+++ scripts/make.sh
@@ -5,3 +5,4 @@
 ### make image
 cd ${QMK_DIR}
 make moonlander:optozorax
+chown -R v:v ${QMK_DIR}
