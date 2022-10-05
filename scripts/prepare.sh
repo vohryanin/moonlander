38e443ab1b07bfdbac894788662b9bcd4b2891c8
Хрянин Владимир <v.xryanin@sigma-it.ru>
2022-10-05 16:05:03 +0700

    fix скрипты модификации QMK

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,6 +4,6 @@
 
 ### modify QMK
 cp ${WORK_DIR}/moonlander/quantum/keymap_common.c ${QMK_DIR}/quantum/keymap_common.c
+cp ${WORK_DIR}/moonlander/quantum/keymap_common.c ${QMK_DIR}/quantum/keymap_common.c
 #cp ${WORK_DIR}/moonlander/tmk_core/common/action.c ${QMK_DIR}/tmk_core/common/action.c
 cp ${WORK_DIR}/moonlander/tmk_core/common/keyboard.h ${QMK_DIR}/tmk_core/common/keyboard.h
-cp ${WORK_DIR}/moonlander/tmk_core/common/rgb_matrix.c ${QMK_DIR}/tmk_core/common/rgb_matrix.c
